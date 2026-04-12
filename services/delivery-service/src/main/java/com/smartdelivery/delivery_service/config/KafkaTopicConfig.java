ee54603a7e137e8e0e6f52f965900174708fbcd4
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-04-12 19:55:45 +0200

    feat(delivery-service): add KafkaConfig, SecurityConfig, WebSocketConfig

---
+++ services/delivery-service/src/main/java/com/smartdelivery/delivery_service/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package com.smartdelivery.delivery_service.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic deliveryUpdatedTopic() {
+        return TopicBuilder.name("delivery.updated")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
